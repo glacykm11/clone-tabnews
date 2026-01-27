88da51de2d9366c4214c534598b7a715cda15a44
Glacy Gomes <glacy.kelly3@gmail.com>
2026-01-27 20:09:02 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Se você for a melhor Tristana da sua escola, dê uma risadinha 😊</h1>
+  return (
+    <h1>Se você for a melhor Tristana da sua escola, dê uma risadinha 😊</h1>
+  );
 }
 
 export default Home;
