d4dd398f3264c553e3e7f966e4c03774d7463353
Glacy Gomes <glacy.kelly3@gmail.com>
2026-06-25 22:32:05 +0000

    removing console.log

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,6 +9,5 @@ test("GET to /api/v1/status should return 200", async () => {
 
   expect(responseBody.dependencies.database.version).toBe("16.0");
   expect(responseBody.dependencies.database.max_connections).toBe(100);
-  console.log(responseBody);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
